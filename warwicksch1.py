28ccf2f00b76a36a70ac04355dec9f2e1ef03555
pikca-c <vincimchan@gmail.com>
2023-10-30 12:35:22 +0000

    modified 1 file

--- warwicksch1.py
+++ warwicksch1.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import pandas as pd 
+import pandas as pd
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome("C:/Users/vinci/Documents/chromedriver-win64/chromedriver.exe")
 driver.get("https://www.warwickschool.org/academic/results")
-driver.find_element_by_css_selector('cookie_information__button ').click()
+driver.find_element_by_css_selector(".cookie_information__close").click()
+
+gcse = driver.find_element(By.CLASS_NAME, ".template.template--accordion")
+strong = gcse.find_elements(By.TAG_NAME, ".strong")
+  
