b55aff7f581d026c95686df59921e85072e61cf3
pikca-c <vincimchan@gmail.com>
2023-10-30 12:07:25 +0000

    added new file

--- warwicksch1.py
+++ warwicksch1.py
@@ -3,4 +3,5 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd 
 
 driver = webdriver.Chrome("C:/Users/vinci/Documents/chromedriver-win64/chromedriver.exe")
-driver.get('https://hoopshype.com/salaries/players/')
+driver.get("https://www.warwickschool.org/academic/results")
+driver.find_element_by_css_selector('cookie_information__button ').click()
