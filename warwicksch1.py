91b091879121bb91170a921e452fc23166828b6f
pikca-c <vincimchan@gmail.com>
2023-11-12 12:40:25 +0000

    updated

--- warwicksch1.py
+++ warwicksch1.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("C:/Users/vinci/Documents/chromedriver-win64/chromedriver.exe")
-driver.get("https://www.warwickschool.org/academic/results")
-driver.find_element_by_css_selector(".cookie_information__close").click()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.cookies": 2})
+driver = webdriver.Chrome("C:/Users/vinci/Documents/chromedriver-win64/chromedriver.exe",chrome_options=chrome_options)
 
-gcse = driver.find_element(By.CLASS_NAME, ".template.template--accordion")
-strong = gcse.find_elements(By.TAG_NAME, ".strong")
+driver.get("https://www.warwickschool.org/academic/results")
+driver.implicitly_wait(10)
+allow_cookies = driver.find_element(By.CLASS_NAME, "cookie_information__button ")
+allow_cookies.click()
+
+gcse = driver.find_element(By.CLASS_NAME, "content__region")
+print (gcse.text)
+
   
