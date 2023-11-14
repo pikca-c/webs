4fe6fe2d161836f5cc552f693878333ad0ffbb7e
pikca-c <vincimchan@gmail.com>
2023-11-14 17:52:48 +0000

    added scraping by using XPATH

---
+++ facebook1.py
@@ -0,0 +1,4 @@
+#find name from post
+#find content from facebook post
+# cs like engineering
+# only without hardware
--- warwickschoolxpath.py
+++ warwickschoolxpath.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-import pandas as pd
 from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
@@ -13,9 +11,5 @@ driver.implicitly_wait(10)
 allow_cookies = driver.find_element(By.CLASS_NAME, "cookie_information__button ")
 allow_cookies.click()
 
-gcse = driver.find_element(By.CLASS_NAME, "content__region")
-print (gcse.text)
-#find my id
-#or find by xpath
-
-  
+results = driver.find_element(By.XPATH, "/html/body/div[2]/main/div[2]/div[2]/p[5]")
+print (results.text)
