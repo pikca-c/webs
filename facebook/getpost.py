ae6e05ab55b5c519c3f628e53cc8b10bcb5f5c8f
pikca-c <vincimchan@gmail.com>
2023-11-22 21:58:08 +0000

    getpost is now formatted

--- facebook/getpost.py
+++ facebook/getpost.py
@@ -1,7 +1,13 @@
 #Credentials required to access the facebook page
 from facebook_scraper import get_posts
 
-for post in get_posts('avantiwestcoastrail', pages=1, credentials=('****@gmail.com', '***'), options={"comments": True}):
-    print(post)
-    print(post['text'])
-    print(post['comments'])
+for post in get_posts('avantiwestcoastrail', pages=1, credentials=('**@gmail.com', '***'), options={"comments": True}):
+    print(f"Date: {post['time']}")
+    print(f"Post Content: {post['text']}")
+    print(f"Post Comments {post['comments']}")
+    no = 0
+    for comment in post['comments_full']:
+        no = no + 1 
+        print(f"Commenter: {comment['commenter_name']}")
+        print(f"Comment Number {no}: {comment['comment_text']}")
+        print("\n")
